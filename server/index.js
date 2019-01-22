fce752c758c2c9b232a2c3d584c157f0aa0bfc77
Anna Miura <annamaliamiura@gmail.com>
2019-01-22 09:07:45 -0800

    Proxy setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const proxy = require('express-http-proxy');
-const app = require('express')();
+const morgan = require('morgan');
+const proxy = require('http-proxy-middleware');
+
+const app = express();
 const port = process.env.PORT || 3010;
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use(express.static(path.join(__dirname, '../src/client')));
+
+app.use(proxy('/api/videos', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+app.use(proxy('/api/games', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
+app.use(proxy('/api/users', {target: 'http://faris-video-service-component-env.paejbawjtv.us-west-2.elasticbeanstalk.com/'}));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
