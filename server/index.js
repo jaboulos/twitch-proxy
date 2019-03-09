c5122b56565fb32cd6a2b8ca5406eb4953d72e4e
Anna Miura <annamaliamiura@gmail.com>
2019-03-09 14:42:02 -0800

    Add deployed urls to index.html and proxy routes to the server for requests

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('newrelic');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const proxy = require('express-http-proxy');
+const proxy = require('http-proxy-middleware');
 const app = require('express')();
 const port = process.env.PORT || 3010;
 app.use(cors());
@@ -10,6 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use(express.static(path.join(__dirname, '../src/client')));
 
+app.use(proxy('/users', {target: 'http://ec2-18-191-220-211.us-east-2.compute.amazonaws.com'}));
+app.use(proxy('/recent-broadcasts', {target: 'http://ec2-54-211-139-47.compute-1.amazonaws.com'}));
+app.use(proxy('/recent-highlights', {target: 'http://ec2-54-211-139-47.compute-1.amazonaws.com'}));
+app.use(proxy('/popular-clips', {target: 'http://ec2-54-211-139-47.compute-1.amazonaws.com'}));
+
+
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
